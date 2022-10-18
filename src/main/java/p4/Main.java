1e88dd920557be89bd17d147aca39cf6bc5fc007
AnthonyLzq <sluzquinosa@uni.pe>
2022-10-18 03:14:15 -0500

    feat: problem 4 finished

---
+++ src/main/java/p4/Main.java
@@ -0,0 +1,107 @@
+package p4;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    JobClient jobClient = new JobClient();
+
+    // Create a configuration object for the job
+    JobConf jobConf = new JobConf(Main.class);
+
+    // Config to run locally
+    jobConf.set("fs.defaultFS", "local");
+    jobConf.set("mapreduce.job.maps", "1");
+    jobConf.set("mapreduce.job.reduces", "1");
+
+    // Set a name of the Job
+    jobConf.setJobName("SalePerCountry");
+
+    // Specify data type of output key and value
+    jobConf.setOutputKeyClass(Text.class);
+    jobConf.setOutputValueClass(Text.class);
+
+    // Specify names of Mapper and Reducer Class
+    jobConf.setMapperClass(CustomMapper.class);
+    jobConf.setReducerClass(CustomReducer.class);
+
+    // Specify formats of the data type of input and output
+    jobConf.setInputFormat(TextInputFormat.class);
+    jobConf.setOutputFormat(TextOutputFormat.class);
+
+    // Set input and output directories using command line arguments,
+    // arg[0] = name of input directory on HDFS, and arg[1] =  name of output directory to be created to store the output file.
+    // FileInputFormat.setInputPaths(jobConf, new Path(args[1]));
+    // FileOutputFormat.setOutputPath(jobConf, new Path(args[2]));
+    // File config to run locally
+
+    Configuration c = new Configuration();
+    String[] files = new GenericOptionsParser(c, args).getRemainingArgs();
+    System.out.println(Arrays.toString(files));
+    FileInputFormat.setInputPaths(jobConf, new Path(files[0]));
+    FileOutputFormat.setOutputPath(jobConf, new Path(files[1]));
+
+    jobClient.setConf(jobConf);
+    try {
+      // run job
+      JobClient.runJob(jobConf);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
+
+class CustomMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
+  @Override
+  public void map(LongWritable key, Text value, OutputCollector<Text, Text> outputCollector, Reporter reporter) throws IOException {
+    if (key.get() == 0 || value.toString().contains("Transaction_date")) return;
+
+    String valueString = value.toString();
+    String[] rowData = valueString.split(",");
+    String country = rowData[7].trim().toLowerCase();
+    float price = Float.parseFloat(rowData[2].trim());
+
+    outputCollector.collect(new Text(country), new Text(country + "-" + price));
+  }
+}
+
+class CustomReducer extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
+  List<String> countries = new ArrayList<>();
+
+  @Override
+  public void reduce(Text key, Iterator<Text> iterator, OutputCollector<Text, Text> outputCollector, Reporter reporter) throws IOException {
+    String countryKey = key.toString();
+
+    if (!countries.contains(countryKey)) {
+      countries.add(countryKey);
+
+      int frequency = 0;
+      float totalPrice = 0;
+
+      // Convert iterator to a list
+      while (iterator.hasNext()) {
+        String[] values = iterator.next().toString().split("-");
+        String country = values[0];
+        float price = Float.parseFloat(values[1]);
+
+        if (country.equals(countryKey)) {
+          totalPrice += price;
+          frequency++;
+        }
+      }
+
+      outputCollector.collect(key, new Text("- average: " + String.format("%.2f", totalPrice/frequency)));
+    }
+  }
+}
