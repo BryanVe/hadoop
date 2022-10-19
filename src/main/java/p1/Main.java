38b4cbece89d7de5f3158451e5f477653a09ee6f
AnthonyLzq <sluzquinosa@uni.pe>
2022-10-18 21:58:47 -0500

    fix: config for problem 1 to run locally

--- src/main/java/p1/Main.java
+++ src/main/java/p1/Main.java
@@ -1,16 +1,26 @@
 package p1;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         JobClient jobClient = new JobClient();
 
         // Create a configuration object for the job
         JobConf jobConf = new JobConf(Main.class);
+
+        // Config to run locally
+        jobConf.set("fs.defaultFS", "local");
+        jobConf.set("mapreduce.job.maps", "1");
+        jobConf.set("mapreduce.job.reduces", "1");
 
         // Set a name of the Job
         jobConf.setJobName("SalePerCountry");
@@ -30,8 +40,14 @@ public class Main {
         // Set input and output directories using command line arguments,
         // arg[0] = name of input directory on HDFS, and arg[1] =  name of output directory to be created to store the output file.
 
-        FileInputFormat.setInputPaths(jobConf, new Path(args[1]));
-        FileOutputFormat.setOutputPath(jobConf, new Path(args[2]));
+        // FileInputFormat.setInputPaths(jobConf, new Path(args[1]));
+        // FileOutputFormat.setOutputPath(jobConf, new Path(args[2]));
+        // File config to run locally
+        Configuration c = new Configuration();
+        String[] files = new GenericOptionsParser(c, args).getRemainingArgs();
+        System.out.println(Arrays.toString(files));
+        FileInputFormat.setInputPaths(jobConf, new Path(files[0]));
+        FileOutputFormat.setOutputPath(jobConf, new Path(files[1]));
 
         jobClient.setConf(jobConf);
         try {
