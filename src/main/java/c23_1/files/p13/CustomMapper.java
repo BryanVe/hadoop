5f8f1f5aa526e2a2cb6bc3545fe916a96eb34305
Bryan Vera <bryan.ve.bv@gmail.com>
2023-04-18 18:01:15 -0500

    feat: p13 solution

---
+++ src/main/java/c23_1/files/p13/CustomMapper.java
@@ -0,0 +1,31 @@
+package c23_1.files.p13;
+
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+import java.io.IOException;
+
+class CustomMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
+  @Override
+  public void map(LongWritable key, Text value, OutputCollector<Text, Text> outputCollector, Reporter reporter) throws IOException {
+    if (key.get() == 0 || value.toString().contains("Transaction_date")) return;
+
+    String valueString = value.toString();
+    String[] rowData = valueString.split(",");
+    String name = rowData[4].trim().toLowerCase();
+
+    if (!name.startsWith("s")) return;
+
+    String paymentType = rowData[3].trim().toLowerCase();
+    String price = rowData[2].trim();
+    Text newKey = new Text(paymentType);
+    Text newValue = new Text(name + "/" + price);
+
+    outputCollector.collect(newKey, newValue);
+  }
+}
---
+++ src/main/java/c23_1/files/p13/CustomReducer.java
@@ -0,0 +1,32 @@
+package c23_1.files.p13;
+
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+
+import java.io.IOException;
+import java.util.*;
+
+class CustomReducer extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
+
+  @Override
+  public void reduce(Text key, Iterator<Text> iterator, OutputCollector<Text, Text> outputCollector, Reporter reporter) throws IOException {
+    float total = 0;
+    List<String> names = new ArrayList<>();
+
+    while (iterator.hasNext()) {
+      Text value = iterator.next();
+      String[] data = value.toString().split("/");
+      String name = data[0];
+      float price = Float.parseFloat(data[1]);
+      total += price;
+      names.add(name);
+    }
+
+    String namesAsString = names.toString();
+    outputCollector.collect(key, new Text(total + " " + namesAsString));
+  }
+}
